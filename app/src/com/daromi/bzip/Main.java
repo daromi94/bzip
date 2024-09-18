1a907b76eea1966e22d6779f9cb55d9fcc7a12ed
daromi94 <davromero07@gmail.com>
2024-09-17 22:36:57 -0500

    fix: :bug: capitalize 'Hello, World!' message

--- app/src/com/daromi/bzip/Main.java
+++ app/src/com/daromi/bzip/Main.java
@@ -3,6 +3,6 @@ package com.daromi.bzip;
 public class Main {
 
   public static void main(String[] args) {
-    System.out.println("Hello, world!");
+    System.out.println("Hello, World!");
   }
 }
